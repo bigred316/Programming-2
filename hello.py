18e17623a8ba330b11dea4f0f1f9de790342152e
bigred316 <price.g@stu.janesville.k12.wi.us>
2025-01-28 20:56:39 +0000

    stuff dot txt

---
+++ hello.py
@@ -0,0 +1,1 @@
+print("me when the")
---
+++ prog52a.py
@@ -0,0 +1,5 @@
+height = int(input("put the length here"))
+width = int(input("do put width here"))
+
+print("perimeter is ", (height + width)*2)
+print("area is ", height*width, " probably")
