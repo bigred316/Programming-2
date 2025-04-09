5a4b095af07525d3a6928e7e1d3f98fa8215ad53
bigred316 <price.g@stu.janesville.k12.wi.us>
2025-04-09 15:09:40 -0500

    4/9

---
+++ cSharp/HelloName/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("hello," +name);
+
+Console.Write("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("You are " + age + " years old");
