af2f5fe3bad2e1b416305788ef73dc0e0d12628c
bigred316 <price.g@stu.janesville.k12.wi.us>
2025-05-16 15:15:23 -0500

    nfdszbgdgfgfd

--- cSharp/pg273bookclubpoints/Form1.cs
+++ cSharp/pg273bookclubpoints/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace prog127
+namespace pg273bookclubpoints
 {
     public partial class Form1 : Form
     {
@@ -19,7 +19,16 @@ namespace prog127
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int books = int.Parse(textBox1.Text);
+            int points = 0;
+            if (books <= 0) points = 0;
+            else if (books == 1) points = 5;
+            else if (books ==2) points = 15;
+            else if (books ==3) points = 30;
+            else if (books >= 4) points = 60;
 
+
+            label2.Text = "Your purchase earned you " + points.ToString() + " points!";
         }
     }
 }
