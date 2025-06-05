ee80114af5820c744529223055426bebe5755d88
bigred316 <price.g@stu.janesville.k12.wi.us>
2025-06-05 13:54:31 -0500

    fggfds

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class MyUserSettings : ApplicationSettingsBase
+        {
+            [UserScopedSetting()]
+            [DefaultSettingValue("white")]
+            public Color BackgroundColor
+            {
+                get
+                {
+                    return ((Color)this["BackgroundColor"]);
+                }
+                set
+                {
+                    this["BackgroundColor"] = (Color)value;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MyUserSettings mus = new MyUserSettings();
+            mus.BackgroundColor = Color.AliceBlue;
+            this.DataBindings.Add(new Binding("BackColor", mus, "BackgroundColor"));
+            mus.Save();
+        }
+        
+    }
+}
---
+++ testingfiles/testingfiles/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testingfiles
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    public class MyUserSettings:ApplicationSettingsBase {
+        [UserScopedSetting()]
+        [DefaultSettingValue("")]
+        public string BackgroundColor
+        {
+            get
+            {
+                return ((string)this["BackgroundColor"]);
+            }
+            set
+            {
+                this["BackgroundColor"] = (string)value;
+            }
+        }
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text =Properties.Settings.Default.Username1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Username1 = textBox1.Text;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MyUserSettings mus = new MyUserSettings();
+            mus.BackgroundColor = "fartballs";
+            this.DataBindings.Add(new Binding("string", mus, "string"));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
